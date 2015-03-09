ebfa502b98b70637791156c1bcc4821f36b21630
Montague Zarzoza <amontaguezarzoza@engr-134-06.eastern.ewu.edu>
2015-03-09 13:30:07 -0700

    version 0

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$('#sky').jQlouds({
+  minClouds: 10, // minimum amount of clouds
+  maxClouds: 15, // maximum amount of clouds
+});
+
+$('#sky').jQlouds({
+  maxWidth: 300, // max image's width
+  minWidth: 200, // amx image's height
+});
+
+$('#sky').jQlouds({
+  wind: true
+});
