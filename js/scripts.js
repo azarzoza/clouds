c3d4e90dd3ae15204f20349b6f94f77820939770
azarzoza <amontaguezarzoza@engr-134-06.eastern.ewu.edu>
2015-03-09 13:34:57 -0700

    version1

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $('#sky').jQlouds({
-  minClouds: 10, // minimum amount of clouds
-  maxClouds: 15, // maximum amount of clouds
+  minClouds: 5, // minimum amount of clouds
+  maxClouds: 10, // maximum amount of clouds
 });
 
 $('#sky').jQlouds({
@@ -11,3 +11,16 @@ $('#sky').jQlouds({
 $('#sky').jQlouds({
   wind: true
 });
+
+
+options = {
+  src: 'img/new-cloud.png', // path to image, the default is a base64 (you can see the actual string in sources)
+  maxWidth: 300, // max image's width
+  minWidth: 200, // amx image's height
+  minClouds: 10, // minimum amount of clouds
+  maxClouds: 20, // maximum amount of clouds
+  skyHeight: null, // height of the container element
+  wind: true // animate clouds, default is false
+};
+
+$('#sky').jQlouds(options);
